2b3b67d363aa79c8def20f57c8cbece320c55522
Daniel Suchan <mr.sucik@gmail.com>
2021-08-22 10:35:50 +0200

    Install script catching bugs

--- client/install.sh
+++ client/install.sh
@@ -1,17 +1,19 @@
+#!/usr/bin/env bash
+
 # Ensure everything is up-to-date
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 
 # Install unclutter to hide the cursor and lxde for its dependencies 
-sudo apt-get install chromium-browser unclutter lxde git
+sudo apt-get install chromium-browser unclutter lxde git --yes
 
 # Download Python script from GitHub
 cd /home/pi/Desktop/
-git clone https://github.com/MrSucik/thesync/tree/master/client
+git clone https://github.com/MrSucik/thesync
 
 # Update /etc/rc.local to run Python script on startup
-mkdir /home/pi/.config/autostart
+mkdir -p /home/pi/.config/autostart
 read -r -d '' PYTHON_STARTUP << EOM
 [Desktop Entry]
 Type=Application
@@ -21,7 +23,7 @@ EOM
 
 sudo echo "$PYTHON_STARTUP" > '/home/pi/.config/autostart/blink.desktop'
 
-# Enable 1: Desktop / CLI and 4: Desktop / Autologin from raspi-config
+# Enable autologin from raspi-config
 
 # Update ~/.config/lxsession/LXDE/autostart to auto-start Chromium
 
@@ -32,5 +34,7 @@ read -r -d '' CHROME_STARTUP << EOM
 @sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium-browser Default/Preferences
 @chromium-browser --noerrdialogs --kiosk https://thesync.web.app/preview/m2TW95XRoJod3tVT8ly5 --incognito --disable-translate
 EOM
+
+sudo mrdir -p /home/pi/.config/lxsession/LXDE/
 
 sudo echo "$CHROME_STARTUP" > '/home/pi/.config/lxsession/LXDE/autostart'
