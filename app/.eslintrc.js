bc4e39cd7bb44cf9488aa3f33275c6dc2a33bab4
Daniel Suchan <mr.sucik@gmail.com>
2021-10-13 21:49:36 +0200

    Add eslint

---
+++ app/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    // Until https://github.com/facebook/react/issues/22545 is fixed
+    // "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
