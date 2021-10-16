54bb34955e3ff93d5083a601691d6ad472f9e205
Daniel Suchan <mr.sucik@gmail.com>
2021-10-16 15:14:15 +0200

    Update env in CI & CD

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -5,13 +5,9 @@ export const czechShortDateFormat = "DD. MM.";
 export const screenHeight = 1760;
 export const screenWidth = 1080;
 
-export const openWeatherApiToken =
-  process.env.REACT_APP_OPEN_WEATHER_API_TOKEN ||
-  process.env.OPEN_WEATHER_API_TOKEN;
-export const firebaseApiKey =
-  process.env.REACT_APP_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY;
-export const firebaseAppId =
-  process.env.REACT_APP_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID;
+export const openWeatherApiToken = process.env.REACT_APP_OPEN_WEATHER_API_TOKEN;
+export const firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+export const firebaseAppId = process.env.REACT_APP_FIREBASE_APP_ID;
 
 console.log(JSON.stringify(process.env));
 
