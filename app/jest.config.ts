c3e51042a23c5f61cd4775e66bd2333b8d6ef779
Daniel Suchan <mr.sucik@gmail.com>
2021-12-06 20:09:25 +0100

    Remove E2E tests from unit test run

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
+  testMatch: ["*.test.ts"],
   modulePaths: ["<rootDir>/src"],
   setupFilesAfterEnv: ["<rootDir>/src/utils/setupTests.ts"],
 };
