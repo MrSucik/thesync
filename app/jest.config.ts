00a29394b02e2fef100431719e5043a4a257ff57
Daniel Suchan <mr.sucik@gmail.com>
2021-12-06 21:00:35 +0100

    Rename E2E folder and remove unit tests

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  testMatch: ["*.test.ts"],
   modulePaths: ["<rootDir>/src"],
   setupFilesAfterEnv: ["<rootDir>/src/utils/setupTests.ts"],
 };
